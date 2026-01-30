34d9829e2803ccac5944db7ebfbb0e236c35fc21
DiegoMendoza21 <128257144+DiegoMendoza21@users.noreply.github.com>
2026-01-29 21:58:51 -0500

    Create Usuarios.jsx

---
+++ Frontend/Pages/Usuarios.jsx
@@ -0,0 +1,10 @@
+const Usuarios = () => {
+  return (
+    <div>
+      <h1>Gestión de Usuarios</h1>
+      <p>Administración de roles y permisos</p>
+    </div>
+  );
+};
+
+export default Usuarios;
