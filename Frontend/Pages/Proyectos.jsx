27709804b20e9730d0c36867a056632c7356dd61
DiegoMendoza21 <128257144+DiegoMendoza21@users.noreply.github.com>
2026-01-29 21:58:32 -0500

    Create Proyectos.jsx

---
+++ Frontend/Pages/Proyectos.jsx
@@ -0,0 +1,10 @@
+const Proyectos = () => {
+  return (
+    <div>
+      <h1>Gestión de Proyectos Docentes</h1>
+      <p>Listado y control de proyectos</p>
+    </div>
+  );
+};
+
+export default Proyectos;
